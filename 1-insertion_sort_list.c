b476b0ca6dce993b47a7e9736a6710ff9c345183
julien3641 <3641@holbertonschool.com>
2022-02-10 17:15:40 +0100

    code update with right and left

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,43 +1,27 @@
 #include "sort.h"
 
-void swapNode(listint_t *first, listint_t *second)
-{
-    listint_t *tempValue;
-
-    tempValue = first;
-    first = second;
-    second = tempValue;
-}
-
 
 void insertion_sort_list(listint_t **list)
 {
     listint_t *tmp;
-    listint_t *infNode;
-    listint_t *back;
+    listint_t *right = NULL;
+    listint_t *left = NULL;
 
     tmp = *list;
     while (tmp != NULL)
     {
-        if (tmp->next->n < tmp->n)
-        {
-            infNode = tmp->next;
-            back = infNode;
-            while (back->prev != NULL)
-            {
-                if (back->prev->prev == NULL)
-                {
-                    infNode->prev = infNode->prev->prev;
-                    infNode->next = infNode->prev->prev->next;
-                }
-                else
-                back = back->prev;
-                if (back->n > back->prev->n)
-                infNode->next = back->next;
-                infNode->prev = back->prev;
-                
-            }
-        }
-        tmp = tmp->next;
+	    right = tmp->next;
+	    left = tmp->prev;
+	    while(tmp->next < left->next)
+	    {
+		    tmp->next = right->next;
+                    right->next->prev = tmp;
+                    right->next = tmp;
+                    tmp->prev = right;
+		    left->next = right;
+		    right->prev = left;
+	    }
+	    tmp = tmp->next;
+	    print_list(*list);
     }
 }
