64ba496948af5353591ff8ca299a2589d083487e
julien3641 <3641@holbertonschool.com>
2022-02-11 14:03:13 +0100

    update code for task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,19 +2,20 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t minimum;
-    int i;
-    int j;
+	size_t minimum, i, j;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        minimum = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[minimum])
-            minimum = j;
-        }
-        swap(&array[minimum], &array[i]);
-        print_array(array, size);
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		minimum = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minimum])
+				minimum = j;
+		}
+		if (array[minimum] != array[i])
+		{
+			swap(&array[minimum], &array[i]);
+			print_array(array, size);
+		}
+	}
 }
