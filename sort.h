8c2a1a92fa0acec224f2f9c7cbfa5f69e9a55f19
Anthony-6 <anthony.lacoste5@gmail.com>
2022-02-13 21:20:45 +0100

    update header file

--- sort.h
+++ sort.h
@@ -25,4 +25,5 @@ void swapNode(listint_t *first, listint_t *second);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void recursion(int *array, int left, int right, size_t size);
+int partition(int *array, int left, int right, size_t size);
 #endif
