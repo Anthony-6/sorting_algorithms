7bcaa685c174d684d2753054bf6adbac93ea757c
julien3641 <3641@holbertonschool.com>
2022-02-13 11:23:25 +0100

    betty style and comments

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,48 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - function that swap to nodes to sort the node
+ * @list: double pointer to the first node
+ * Return: Nothing if it fail
+ */
+
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *right = NULL;
-    listint_t *left = NULL;
-    listint_t *tmp = *list;
+	listint_t *right = NULL;
+	listint_t *left = NULL;
+	listint_t *tmp = *list;
 
-    if (*list == NULL)
-	    return;
+	if (*list == NULL)
+		return;
 
-    tmp = (*list)->next;
-    while (tmp != NULL)
-    {
-	    left = tmp->prev;
-	    right = tmp->next;
-	    while(left && tmp->n < left->n)
-	    {
-		    if (left->prev != NULL)
-			    left->prev->next = tmp;
-		    left->next = right;
-		    tmp->next = left;
-		    tmp->prev = left->prev;
-		    left->prev = tmp;
-		    if (right != NULL)
-			    right->prev = left;
-		    right = left;
-		    left = tmp->prev;
-		    if (left == NULL)
-		    {
-			    *list = tmp;
-			    tmp = tmp->next;
-			    print_list(*list);
-			    break;
-		    }
-		    print_list(*list);
-	    }
-	    if (left != NULL)
-		    tmp = right;
-    }
+	tmp = (*list)->next;
+	while (tmp != NULL)
+	{
+		left = tmp->prev;
+		right = tmp->next;
+		while (left && tmp->n < left->n)
+		{
+			if (left->prev != NULL)
+				left->prev->next = tmp;
+			left->next = right;
+			tmp->next = left;
+			tmp->prev = left->prev;
+			left->prev = tmp;
+			if (right != NULL)
+				right->prev = left;
+			right = left;
+			left = tmp->prev;
+			if (left == NULL)
+			{
+				*list = tmp;
+				tmp = tmp->next;
+				print_list(*list);
+				break;
+			}
+			print_list(*list);
+		}
+		if (left != NULL)
+			tmp = right;
+	}
 }
