50c78c389f33e0e44034ea79a1e6d18f93782b49
julien3641 <3641@holbertonschool.com>
2022-02-14 15:31:33 +0100

    change malloc

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,29 +10,28 @@
 
 void counting_sort(int *array, size_t size)
 {
-	int max, i;
+	int max = array[0], i;
 	int *count, *output;
 
 	if (size < 2)
-		return;
-	max = array[0];
-	for (i = 1; i < (int)size; i++)
-		if (array[i] > max)
-			max = array[i];
+                return;
+        for (i = 1; i < (int)size; i++)
+                if (array[i] > max)
+                        max = array[i];
 	count = malloc(sizeof(int) * max);
 	if (count == NULL)
 		return;
-
-	for (i = 0; i <= max + 1; i++)
-		count[i] = 0;
-	for (i = 0; i < (int)size; i++)
-		count[array[i]]++;
-	for (i = 1; i <= max + 1; i++)
-		count[i] += count[i - 1];
-	print_array(count, max + 1);
 	output = malloc(sizeof(int) * size);
 	if (output == NULL)
 		return;
+	max = max + 1;
+	for (i = 0; i <= max; i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]]++;
+	for (i = 1; i <= max; i++)
+		count[i] += count[i - 1];
+	print_array(count, max);
 	for (i = (int)size - 1; i >= 0; i--)
 	{
 		output[count[array[i]] - 1] = array[i];
