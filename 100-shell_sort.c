d87795e166f589c05be442b5f9848e4cf5ae9eae
julien3641 <3641@holbertonschool.com>
2022-02-14 11:44:07 +0100

    betty style

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -14,23 +14,23 @@ void shell_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 
-	while(gap < size / 3)
-		gap = (gap * 3) +1;
+	while (gap < (size / 3))
+		gap = (gap * 3) + 1;
 
-	while(gap > 0)
+	while (gap > 0)
 	{
-		for(i = gap; i < size; i++)
+		for (i = gap; i < size; i++)
 		{
 			tmp = array[i];
 			j = i;
-			while(j > gap -1 && array[j - gap] >= tmp)
+			while (j > gap - 1 && array[j - gap] >= tmp)
 			{
 				array[j] = array[j - gap];
 				j -= gap;
 			}
 			array[j] = tmp;
 		}
-		gap = (gap - 1) /3;
+		gap = (gap - 1) / 3;
 		print_array(array, size);
 	}
 }
