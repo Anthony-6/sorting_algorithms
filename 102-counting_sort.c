bae3aaaa67fd868a65fc556095f61c9bb2ece7cc
julien3641 <3641@holbertonschool.com>
2022-02-14 15:49:29 +0100

    fix bug

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,32 +10,33 @@
 
 void counting_sort(int *array, size_t size)
 {
-	int max = array[0], i;
+	int max, i;
 	int *count, *output;
 
 	if (size < 2)
 		return;
+	max = array[0];
 	for (i = 1; i < (int)size; i++)
 		if (array[i] > max)
 			max = array[i];
+	max = max + 1;
 	count = malloc(sizeof(int) * max);
 	if (count == NULL)
 		return;
 	output = malloc(sizeof(int) * size);
 	if (output == NULL)
 		return;
-	max = max + 1;
 	for (i = 0; i <= max; i++)
 		count[i] = 0;
 	for (i = 0; i < (int)size; i++)
-		count[array[i]]++;
+		count[array[i]] += 1;
 	for (i = 1; i <= max; i++)
 		count[i] += count[i - 1];
 	print_array(count, max);
 	for (i = (int)size - 1; i >= 0; i--)
 	{
 		output[count[array[i]] - 1] = array[i];
-		count[array[i]]--;
+		count[array[i]] -= 1;
 	}
 	for (i = 0; i < (int)size; i++)
 		array[i] = output[i];
