046237c85fbc2ee14c4924d6d6daba2a4547d36a
julien3641 <3641@holbertonschool.com>
2022-02-14 11:41:03 +0100

    add shell sort file and update sort.h

---
+++ 100-shell_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts in ascending order
+ * @array: pointer to the array
+ * @size: size of the array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+	int tmp;
+
+	if (size < 2)
+		return;
+
+	while(gap < size / 3)
+		gap = (gap * 3) +1;
+
+	while(gap > 0)
+	{
+		for(i = gap; i < size; i++)
+		{
+			tmp = array[i];
+			j = i;
+			while(j > gap -1 && array[j - gap] >= tmp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = tmp;
+		}
+		gap = (gap - 1) /3;
+		print_array(array, size);
+	}
+}
